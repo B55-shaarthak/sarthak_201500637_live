d8cf543399fa17eca1df43851fc4ab7cf7bdd95f
B55-shaarthak <80021092+B55-shaarthak@users.noreply.github.com>
2022-07-11 18:59:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("name").innerHTML="Shaarthak Singh";
+document.getElementById("roll-no").innerHTML="201500637";
+document.getElementById("university").innerHTML="GLA University, Mathura";
+
+var date=new Date();
+
+document.getElementById("date").innerHTML=date.toDateString(); 
